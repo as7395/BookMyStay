3163bcfc0642d827d1e7fd3a4a4e7ec85b353df3
as7395 <as7395@srmist.edu.in>
2026-04-15 10:18:18 +0530

    Avinash added U1

---
+++ App/src/BookMyStay.java
@@ -0,0 +1,26 @@
+/**
+ * The UseCase1HotelBookingApp class serves as the entry point for the 
+ * Hotel Booking Management System. This initial version demonstrates 
+ * basic application startup and console interaction.
+ * * @author Gemini
+ * @version 1.0
+ */
+public class BookMyStay {
+
+    /**
+     * The main method is the execution starting point invoked by the JVM.
+     * It outputs a welcome message and version details to the console.
+     * * @param args Command-line arguments (not used in this version).
+     */
+    public static void main(String[] args) {
+
+        // Displaying the application name and version using String literals
+        System.out.println("--------------------------------------------------");
+        System.out.println("Welcome to the Book My Stay App!");
+        System.out.println("Application: Hotel Booking Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("--------------------------------------------------");
+
+        // The application terminates automatically after this point
+    }
+}
